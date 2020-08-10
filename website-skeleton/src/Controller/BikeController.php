fe5dfebaaf9255445b0a59cb7d8e1745f65d8352
Grivznof <pbaillet9@gmail.com>
2020-08-10 21:52:07 +0200

    bike
    + bike entity
    + bike form
    + bike frontend show

---
+++ website-skeleton/src/Controller/BikeController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Bike;
+use App\Form\BikeType;
+use App\Repository\BikeRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BikeController extends AbstractController
+{
+    /**
+     * @Route("/velo", name="bike")
+     */
+    public function index(BikeRepository $repo)
+    {
+        $bikes = $repo->findAll();
+
+        return $this->render('bike/index.html.twig', [
+            'controller_name' => 'BikeController',
+            'bikes' => $bikes,
+        ]);
+    }    
+    
+    /**
+    * @Route("/velo/ajouter", name="bike_add")
+    */
+   public function bikeAddForm(Request $request)
+   {
+
+        $bike = new Bike();
+
+        $form = $this->createForm(BikeType::class, $bike); 
+
+        $em = $this->getDoctrine()->getManager();
+
+        $form-> handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em->persist($bike);
+            $em->flush();
+            
+        }
+
+
+       return $this->render('bike/add.html.twig', [
+           'controller_name' => 'BikeController',
+           'form' => $form->createView(),
+       ]);
+   }
+}
